c6e859e63f381c98a9840a8e8b3569ad0c82d6c8
Billy <billybright1992@gmail.com>
2019-06-26 18:30:40 -0700

    gets users input add check if matched to one of the letters4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,9 @@
-// Whenever a key is pressed, alert "pressed a button".
-document.onkeyup = function() {  
-};
-    var wins = 0;
-    var loss = 0;
-    var animals = ["hippopotamus", "chimpanzee", "crocodile", "scorpion", "alligator", "dolphin"];
-    var letter = " ";
-    var randWord = getRanWord();
+var wins = 0;
+var loss = 0;
+var animals = ["hippopotamus", "chimpanzee", "crocodile", "scorpion", "alligator", "dolphin"];
+var letter = " ";
+var randWord = getRanWord();
+
 
     console.log(randWord);
     
@@ -13,10 +11,26 @@ document.onkeyup = function() {
     var rand = animals[Math.floor(Math.random()* animals.length)];
     return rand;
   }
- 
   function display() {
       for(i = 0; i <= randWord.length; i++){
         letter = letter + "_" + " ";
     }
     document.getElementById('secretWord').innerHTML = "<h3>" + letter + "</h3>";
   }
+  function match(guess) {
+    for(i = 0; i <= randWord.length; i++){
+        if(guess === randWord[i]) {
+            
+            console.log(randWord[i])
+        }
+    }
+  }
+  function getLetter() {
+    var userGuess = prompt("Choose a letter");
+    return userGuess;
+  }
+// Whenever a key is pressed, alert "pressed a button".
+document.onkeyup = function() {  
+    var guess = getLetter();
+    match(guess);
+};
